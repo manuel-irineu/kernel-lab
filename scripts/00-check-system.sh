bcf63a311ef8b2111abb03d6c2101e9b1bb8b4fe
manuel_irineu <irineubsd@gmail.com>
2026-07-16 00:28:27 -0300

    refactor: remove dkms probe from inventory

--- scripts/00-check-system.sh
+++ scripts/00-check-system.sh
@@ -26,7 +26,6 @@ run_optional 'Debian release' cat /etc/os-release
 run_optional 'Boot command line' cat /proc/cmdline
 run_optional 'PCI devices and drivers' lspci -nnk
 run_optional 'Session type' printenv XDG_SESSION_TYPE
-run_optional 'DKMS status' dkms status
 run_optional 'Loaded modules' lsmod
 run_optional 'Disk usage' df -h
 run_optional 'Memory' free -h
